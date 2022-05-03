2fa75f0087e104dd6e9d5b800a93347edb3b5d8c
Talina <tshabliy@gmail.com>
2022-05-03 13:59:59 +0300

    forms implemented

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import "./slider";
 import modals from './modules/modals';
 import tabs from "./modules/tabs";
-
+import forms from "./modules/forms";
 
 window.addEventListener('DOMContentLoaded', () => {
+    "use strict";
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    forms();
 });
